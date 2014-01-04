b396b79512a64bfe1bad5c3d0c1572d5572c5d69
MonikaBuergi <monika.buergi@students.fhv.at>
2014-01-04 12:26:46 +0100

    Webservice client erstellen

---
+++ TTTWebservice/TTTWebservice-ejb/src/java/webservice/TTTwebservice.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package webservice;
+
+import javax.jws.WebService;
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.ejb.Stateless;
+
+/**
+ *
+ * @author Monika
+ */
+@WebService(serviceName = "NewWebService")
+@Stateless()
+public class TTTwebservice {
+
+    /**
+     * This is a sample web service operation
+     */
+    @WebMethod(operationName = "hello")
+    public String hello(@WebParam(name = "name") String txt) {
+        return "Hello " + txt + " !";
+    }
+}
